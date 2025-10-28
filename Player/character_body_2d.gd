b8d3c180f3127716e7464772884f8c89135ede0e
Fredrik <fksvendsrud@gmail.com>
2025-10-28 19:27:13 +0100

    fixed main

---
+++ Player/character_body_2d.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED := 300.0
+const ROTATION_SPEED := 3.0
+
+func _physics_process(delta: float) -> void:
+	var rotation_input := Input.get_action_strength("right") - Input.get_action_strength("left")
+	rotation += rotation_input * ROTATION_SPEED * delta
+	
+	var forward_input := Input.get_action_strength("back") - Input.get_action_strength("forward")
+	print(forward_input)
+	var direction := Vector2.RIGHT.rotated(rotation)  # Character faces along X-axis by default
+	velocity = direction * forward_input * SPEED
+	
+	
+	move_and_slide()
+
+	
