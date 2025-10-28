89b2bfe6a110985556aca9e1aa0f6086e3789e45
Aks-l <wiersdalenaksel@gmail.com>
2025-10-28 19:23:12 +0100

    Initial main game scene

--- Furniture/Seating/table.gd
+++ Furniture/Seating/table.gd
@@ -1,1 +1,18 @@
 extends StaticBody2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+@export var capacity: int = 4
+@export var occupants: int = 0
+
+func _ready():
+	pass
+
+func _occupy(entity):
+	if capacity == occupants:
+		push_error("Table full")
+		return
+	occupants += 1
+
+func exit(entity):
+	occupants -= 1
