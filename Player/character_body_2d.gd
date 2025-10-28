88279d157b799f0d3fc92b6297ec3d2e70557c5e
Fredrik <fksvendsrud@gmail.com>
2025-10-28 19:50:19 +0100

    feat (interactions): Implemneted a system to allow the player to interact with objects

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -8,13 +8,23 @@ func _physics_process(delta: float) -> void:
 	var rotation_input := Input.get_action_strength("right") - Input.get_action_strength("left")
 	rotation += rotation_input * ROTATION_SPEED * delta
 	
-	var forward_input := Input.get_action_strength("back") - Input.get_action_strength("forward")
-	print(forward_input)
-	var direction := Vector2.RIGHT.rotated(rotation)  # Character faces along X-axis by default
-	velocity = direction * forward_input * SPEED
-
+	var forward_input := Input.get_action_strength("forward") - Input.get_action_strength("back")
 	
+
+
+	var direction := Vector2.RIGHT.rotated(rotation)  # Character faces along X-axis by default
+	if forward_input == -1:
+		velocity = direction * forward_input * (SPEED / 2)
+	else:
+		velocity = direction * forward_input * SPEED
 	
 	move_and_slide()
 
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact"):
+		interact_with_target()
+	
+func interact_with_target():
+	if $InteractRayCast2D.is_colliding():
+		print("found something")
 	
